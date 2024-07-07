932ed5524a3779686e1f7b4877a9c1e0f9e2b2b3
PehlivanMert <pehlivannmert@gmail.com>
2024-07-07 15:38:42 +0300

    Debug ok

--- src/main/java/dev/pehlivan/financeandinvesmentmanagementapplication/repository/BudgetRepository.java
+++ src/main/java/dev/pehlivan/financeandinvesmentmanagementapplication/repository/BudgetRepository.java
@@ -3,9 +3,11 @@ package dev.pehlivan.financeandinvesmentmanagementapplication.repository;
 import dev.pehlivan.financeandinvesmentmanagementapplication.model.Budget;
 import dev.pehlivan.financeandinvesmentmanagementapplication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
     List<Budget> findByUser(User user);
 }
--- src/main/java/dev/pehlivan/financeandinvesmentmanagementapplication/repository/NotificationRepository.java
+++ src/main/java/dev/pehlivan/financeandinvesmentmanagementapplication/repository/NotificationRepository.java
@@ -3,9 +3,11 @@ package dev.pehlivan.financeandinvesmentmanagementapplication.repository;
 import dev.pehlivan.financeandinvesmentmanagementapplication.model.Notification;
 import dev.pehlivan.financeandinvesmentmanagementapplication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByUser(User user);
 }
